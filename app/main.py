112e90a6d589efdaf2c7bae408b802bee0f17fa9
tashlykovamur1 <wait-until@yandex.ru>
2025-03-20 02:19:44 +0500

    Добавила Dockerfile и возможность запускать сервис из докера

--- app/main.py
+++ app/main.py
@@ -1,19 +1,32 @@
+import logging
+from contextlib import asynccontextmanager
+
 import dotenv
+from fastapi_pagination import add_pagination
+
 dotenv.load_dotenv()
 
-from fastapi_pagination import add_pagination
+
 import uvicorn
 from fastapi import FastAPI
+
+from app.routers import status, users
 from app.database.engine import create_db_and_tables
-from routers import status, users
 
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    logging.warning("On startup")
+    create_db_and_tables()
+    yield
+    logging.warning("On shutdown")
+
+
+app = FastAPI(lifespan=lifespan)
 app.include_router(status.router)
 app.include_router(users.router)
 add_pagination(app)
 
 if __name__ == "__main__":
-    create_db_and_tables()
     uvicorn.run(app, host="localhost", port=8002)
 
